87b65891f660d00ad044540a0777fa9a9cb1d8ac
Nuri Okumuş <nuriokumus@192.168.1.108>
2025-06-28 15:45:58 +0300

    Initialize SwiftUI project

---
+++ AIChatSignAssistant/AIChatSignAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatSignAssistantApp.swift
+//  AIChatSignAssistant
+//
+//  Created by Nuri Okumuş on 28.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct AIChatSignAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIChatSignAssistant/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AIChatSignAssistant
+//
+//  Created by Nuri Okumuş on 28.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
